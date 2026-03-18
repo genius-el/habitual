437d84dd2590f80d5d5b0ab2c6e8dde7a43f8454
genius-el <elvismichael888@gmail.com>
2026-03-18 14:42:11 +0100

    feat: Add "clear all" entries button and function
    Permit user to clear all entries from previous
    usage

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-'use strict';
+"use strict";
 
 // RELEVANT DOM REFERENCES
 // Field References
-const urgentAndImportantInput = document.getElementById('urgAndImpField');
-const importantInput = document.getElementById('impField');
-const urgentInput = document.getElementById('urgField');
-const nilInput = document.getElementById('nilField');
+const urgentAndImportantInput = document.getElementById("urgAndImpField");
+const importantInput = document.getElementById("impField");
+const urgentInput = document.getElementById("urgField");
+const nilInput = document.getElementById("nilField");
 
 // Button References
-const urgentAndImportantBtn = document.getElementById('urgAndImpBtn');
-const importantBtn = document.getElementById('impBtn');
-const urgentBtn = document.getElementById('urgBtn');
-const nilBtn = document.getElementById('nilBtn');
+const urgentAndImportantBtn = document.getElementById("urgAndImpBtn");
+const importantBtn = document.getElementById("impBtn");
+const urgentBtn = document.getElementById("urgBtn");
+const nilBtn = document.getElementById("nilBtn");
+
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 // View Reference
-const habitsList = document.getElementById('habits');
+const habitsList = document.getElementById("habits");
 
 // Other Variables
 // let habits = [];
@@ -25,173 +27,114 @@ let habits;
 const storedHabits = localStorage.getItem("habits");
 
 if (storedHabits !== null) {
-    habits = JSON.parse(storedHabits);
+  habits = JSON.parse(storedHabits);
 } else {
-    habits = [];
+  habits = [];
 }
 
-
 let habit = {
-    id: '',
-    task: '',
-    category: '',
+  id: "",
+  task: "",
+  category: "",
 };
 
 // EVENT LISTENERS THE ADD BUTTON FOR EACH HABIT CATEGORY
-urgentAndImportantBtn.addEventListener('click', function() {
-    const urgentAndImportantInputValue = urgentAndImportantInput.value;
-    console.log(urgentAndImportantInputValue);
+urgentAndImportantBtn.addEventListener("click", function () {
+  const urgentAndImportantInputValue = urgentAndImportantInput.value;
+  console.log(urgentAndImportantInputValue);
 
-    if (urgentAndImportantInputValue) {
-        habit = {
-            id: Date.now(),
-            task: urgentAndImportantInputValue,
-            category: 'is Urgent and Important'
-        };
-        habits.push(habit);
-        console.log(habits);
-        habitDisplay();
-    }
-    // Saving user data 
-    localStorage.setItem("habits", JSON.stringify(habits));
+  if (urgentAndImportantInputValue) {
+    habit = {
+      id: Date.now(),
+      task: urgentAndImportantInputValue,
+      category: "is Urgent and Important",
+    };
+    pushHabit();
+  }
 });
 
-importantBtn.addEventListener('click', function() {
-    const importantInputValue = importantInput.value;
-    console.log(importantInputValue);
+importantBtn.addEventListener("click", function () {
+  const importantInputValue = importantInput.value;
+  console.log(importantInputValue);
 
-    if (importantInputValue) {
-            habit = {
-                id: Date.now(),
-                task: importantInputValue,
-                category: 'is Important'
-            }
-            habits.push(habit);
-            console.log(habits);
-            habitDisplay();
-    }
-
-
+  if (importantInputValue) {
+    habit = {
+      id: Date.now(),
+      task: importantInputValue,
+      category: "is Important",
+    };
+    pushHabit();
+  }
 });
 
-urgentBtn.addEventListener('click', function() {
-    const urgentInputValue = urgentInput.value;
-    console.log(urgentInputValue);
+urgentBtn.addEventListener("click", function () {
+  const urgentInputValue = urgentInput.value;
+  console.log(urgentInputValue);
 
-    if (urgentInputValue) {
-            habit = {
-                id: Date.now(),
-                task: urgentInputValue,
-                category: 'is Urgent',
-            }
-            habits.push(habit);
-            console.log(habits);
-            habitDisplay();
-    }
-
+  if (urgentInputValue) {
+    habit = {
+      id: Date.now(),
+      task: urgentInputValue,
+      category: "is Urgent",
+    };
+    pushHabit();
+  }
 });
 
-nilBtn.addEventListener('click', function() {
-    const nilInputValue = nilInput.value;
-    console.log(nilInputValue);
+nilBtn.addEventListener("click", function () {
+  const nilInputValue = nilInput.value;
+  console.log(nilInputValue);
 
-    if (nilInputValue) {
-            habit = {
-                id: Date.now(),
-                task: nilInputValue,
-                category: 'is Not Urgent and Not Important'
-            }
-            habits.push(habit);
-            console.log(habits);
-            habitDisplay();
-    }
-
+  if (nilInputValue) {
+    habit = {
+      id: Date.now(),
+      task: nilInputValue,
+      category: "is Not Urgent and Not Important",
+    };
+    pushHabit();
+  }
 });
 
 // HabitsDisplay Function
-const habitDisplay = function() {
-    // saving user data to local storage
-    localStorage.setItem("habits", JSON.stringify(habits)); // save first
-    habitsList.innerHTML = "";
+const habitDisplay = function () {
+  // saving user data to local storage
+  localStorage.setItem("habits", JSON.stringify(habits)); // save first
+  habitsList.innerHTML = "";
 
-    habits.forEach((h) => {
-        const li = document.createElement('li');
-        li.style.marginTop = '-5px';
-        li.innerHTML = `
+  habits.forEach((h) => {
+    const li = document.createElement("li");
+    li.style.marginTop = "-5px";
+    li.innerHTML = `
         <span>${h.task} ${h.category}</span>
-        <button class="delete-btn" onclick="habitDelete(${h.id})">Delete</button>`
+        <button class="delete-btn" onclick="habitDelete(${h.id})">Delete</button>`;
 
-        habitsList.appendChild(li);
-    })
-}
+    habitsList.appendChild(li);
+  });
+};
 
 // Habit Delete Functionality
-const habitDelete = function(id) {
-    habits = habits.filter((h) => {
-        return h.id !== id
-    })
+const habitDelete = function (id) {
+  habits = habits.filter((h) => {
+    return h.id !== id;
+  });
 
+  habitDisplay();
+};
+
+// Push Habit to Array functionality
+const pushHabit = function () {
+  if (habits.length < 8) {
+    habits.push(habit);
     habitDisplay();
-}
+  } else {
+    alert("You have reached the maximum of 8 habits");
+  }
+};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Display Habits Function
-// const habitDisplay = function () {
-//     habitsList.innerHTML = " ";
-
-//     habits.forEach((h) => {
-//         const li = document.createElement("li");
-//         li.innerHTML = `
-//             <span>${h.task} ${h.category}</span>
-//             <button class="delete-btn" onclick="deleteHabit(${h.id})">Delete</button>
-//             `;
-
-//         habitsList.appendChild(li);
-//     })
-// }
+// The Clear All Functionality
+clearAllBtn.addEventListener("click", function () {
+    habits = [];
+    localStorage.removeItem("habits");
+    habitDisplay();
+});
